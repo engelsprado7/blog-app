687820c07456896ee8ec0524ea8793bd66167dbc
Engels Prado Herrera <99674354+engelsprado7@users.noreply.github.com>
2024-08-03 02:07:34 -0600

    Update server.js

--- server.js
+++ server.js
@@ -39,8 +39,7 @@ app.use(
         saveUninitialized: false,
         cookie: {
             maxAge: 1000 * 60 * 60 * 24, // 1 day
-            sameSite: 'none',
-            secure: 'auto'
+            sameSite: true
         }
     })
 );
