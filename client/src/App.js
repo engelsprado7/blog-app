f534cc4c7bdd71cb6c8db5b4f960b34341137a2f
engelsprado7 <99674354+engelsprado7@users.noreply.github.com>
2024-08-02 20:57:20 -0600

    relatives path

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import PostForm from './components/postForm';
-import Register from './components/register';
-import Login from './components/login';
-import PostList from './components/posts';
+import PostForm from 'components/postForm';
+import Register from 'components/register';
+import Login from 'components/login';
+import PostList from 'components/posts';
 const App = () => (
   <Router>
     <div className="App">
