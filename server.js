cb0e2d34a0cfcc0c8ffd788c31888b73ce171fa5
engelsprado7 <99674354+engelsprado7@users.noreply.github.com>
2024-08-03 12:01:52 -0600

    add secure

--- server.js
+++ server.js
@@ -39,8 +39,6 @@ app.use(
         saveUninitialized: false,
         cookie: {
             maxAge: 1000 * 60 * 60 * 24, // 1 day
-            sameSite: 'none',
-            secure: true
         }
     })
 );
